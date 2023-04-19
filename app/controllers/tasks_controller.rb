51a5fcd300babff39a55714c8c12a35173b7225f
nogi kosuke <kskng0902@gmail.com>
2023-04-19 17:57:08 +0900

    編集機能、新規作成機能の追加、フラッシュメッセージ導入

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,37 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:notice] = "タスクを新規登録しました"
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:notice] = "タスクを編集しました。"
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
 end
