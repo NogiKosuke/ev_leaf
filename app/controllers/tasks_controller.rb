ec5ccdf1df877b88060667207eace865356b1278
nogi kosuke <kskng0902@gmail.com>
2023-04-26 00:44:30 +0900

    ログイン機能の実装。ログアウト機能の実装。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,21 +4,21 @@ class TasksController < ApplicationController
       title = params[:task][:title]
       status = params[:task][:status]
       if title.present? && (status != "")
-        @tasks = Task.title_like(title).status_where(status).page(params[:page]).per(15)
+        @tasks = current_user.tasks.title_like(title).status_where(status).page(params[:page]).per(15)
       elsif title.present? && (status == "")
-        @tasks = Task.title_like(title).page(params[:page]).per(15)
+        @tasks = current_user.tasks.title_like(title).page(params[:page]).per(15)
       elsif title.blank? && (status != "")
-        @tasks = Task.status_where(status).page(params[:page]).per(15)
+        @tasks = current_user.tasks.status_where(status).page(params[:page]).per(15)
       else
         redirect_to tasks_path
       end
     else
       if params[:sort_expired].present?
-        @tasks = Task.all.order(expired_at: :asc).page(params[:page]).per(15)
+        @tasks = current_user.tasks.order(expired_at: :asc).page(params[:page]).per(15)
       elsif params[:sort_priority].present?
-        @tasks = Task.all.order(priority: :asc).page(params[:page]).per(15)
+        @tasks = current_user.tasks.order(priority: :asc).page(params[:page]).per(15)
       else
-        @tasks = Task.all.order(created_at: :desc).page(params[:page]).per(15)
+        @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(15)
       end
     end
   end
@@ -32,7 +32,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:notice] = "タスクを新規登録しました"
       redirect_to tasks_path
