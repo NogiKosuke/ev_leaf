d95b46f39242630429c38ebfff0acce43c48cab4
nogi kosuke <kskng0902@gmail.com>
2023-04-25 23:49:47 +0900

    ユーザーモデルの作成。タスクとの紐付け

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,5 @@ class Task < ApplicationRecord
   scope :title_like, -> (title){ where('title like ?', "%#{title}%") }
   scope :status_where, -> (status){ where('status like ?', "#{status}") }
   enum priority: { '高': 0, '中': 1 , '低': 2 }
+  belongs_to :user
 end
