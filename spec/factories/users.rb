e18257307d422822a3cc125d0e531e07ec3726c7
nogi kosuke <kskng0902@gmail.com>
2023-04-27 01:19:44 +0900

    テストコードを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :user do
-    
+    name { 'user' }
+    email { 'aaa@aaa.aaa' }
+    password { 'password' }
+    password_confirmation { 'password' }
+    admin { true }
+  end
+
+  factory :second_user, class: User do
+    name { 'alice' }
+    email { 'bbb@bbb.bbb' }
+    password { 'password' }
+    password_confirmation { 'password' }
+    admin { false }
   end
 end
